3dcf7f53b2f6eff1c10f50931ae3addb14459094
kaku <ghjcumt2008@gmail.com>
2013-04-12 18:23:29 +0900

    bug fix

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -1,9 +1,9 @@
 ActiveAdmin.register Article do
   index do
     column :title do |article|
-        link_to article.name, [:admin, article]
+        link_to article.title, [:admin, article]
     end
-    column :creaet_at
+    column :created_at
    
     default_actions
   end
