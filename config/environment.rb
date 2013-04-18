94e11c28a17468f5e675f570591b9310a0bda0da
kaku <ghjcumt2008@gmail.com>
2013-04-18 18:45:23 +0900

    add sheet style

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Blog::Application.initialize!
+if not $SPEEDUP_LIBCACHE and ENV['RAILS_ENV'] != 'production'
+  # speedup Rails 3 startup by 30% on Windows
+  $SPEEDUP_LIBCACHE = {}
+  module Kernel
+    alias chained_require require
+    def require(path)
+      $SPEEDUP_LIBCACHE[path] ||= chained_require path
+    end
+    private :require
+    private :chained_require
+  end
+end
